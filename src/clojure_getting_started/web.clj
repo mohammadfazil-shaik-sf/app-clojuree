39a99b03968f837f67f37a9fff074962a2c89639
mohammadfazil-shaik-sf <mohammadfazil.shaik@heroku.com>
2020-05-11 12:35:40 +0530

    f

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -6,14 +6,11 @@
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]
             [camel-snake-kebab.core :as kebab]))
-(def sample (env :sample "sample-string-thing"))
 
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body (for [kind ["camel" "snake" "kebab"]]
-           (format "<a href=\"/%s?input=%s\">%s %s</a><br />"
-                   kind sample kind sample))})
+   :body "Hello from Heroku"})
 
 (defroutes app
 		(GET "/camel" {{input :input} :params}
